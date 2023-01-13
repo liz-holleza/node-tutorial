de304e96f3c9cf9252ad9ff8f64ee3fa994ace92
Lizlie Holleza <lizlie.holleza@awsys-i.com>
2023-01-13 15:49:43 +0800

    first commit

---
+++ 9-path-module.js
@@ -0,0 +1,14 @@
+const { default: test } = require('node:test');
+const path = require('path');
+
+console.log(path.sep)
+
+const filePath = path.join('/content', 'subfolder', 'test.txt')
+console.log(filePath)
+
+
+const base = path.basename(filePath)
+console.log(base)
+
+const absolute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt')
+console.log(absolute)
